3bfed092a0738c0317acb1193c512f980fd82a1c
Poh Keong <angpkeong@gmail.com>
2021-08-21 15:10:35 +0800

    Add required field for login and register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -40,6 +40,7 @@ function Login() {
                 type="email"
                 name="email"
                 id="email"
+                required
                 placeholder="Your Email"
                 class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
               />
@@ -62,6 +63,7 @@ function Login() {
               <input
                 type="password"
                 name="password"
+                required
                 id="password"
                 placeholder="Your Password"
                 class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
--- src/pages/PartnerRegister.js
+++ src/pages/PartnerRegister.js
@@ -23,30 +23,32 @@ function PartnerRegister() {
           <form className="mt-8 space-y-6 mx-10" action="#" method="POST">
             <div class="mb-6">
               <label
-                for="email"
+                for="firstname"
                 class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
               >
                 First Name
               </label>
               <input
-                type="email"
-                name="email"
-                id="email"
+                type="firstname"
+                name="firstname"
+                id="firstname"
+                required
                 placeholder="Your First Name"
                 class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
               />
             </div>
             <div class="mb-6">
               <label
-                for="email"
+                for="lastname"
                 class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
               >
                 Last Name
               </label>
               <input
-                type="email"
-                name="email"
-                id="email"
+                type="lastname"
+                name="lastname"
+                id="lastname"
+                required
                 placeholder="Your Last Name"
                 class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
               />
@@ -62,6 +64,7 @@ function PartnerRegister() {
                 type="email"
                 name="email"
                 id="email"
+                required
                 placeholder="Your Email"
                 class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
               />
@@ -79,6 +82,7 @@ function PartnerRegister() {
                 type="password"
                 name="password"
                 id="password"
+                required
                 placeholder="Your Password"
                 class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
               />
