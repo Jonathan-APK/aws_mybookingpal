fe2e003133d75aac2c6cf287d8b179a7919b3f09
DinDragon <91307218+DinDragon@users.noreply.github.com>
2021-10-07 03:47:04 +0800

    Update FacilityDetails.js

--- src/pages/user/FacilityDetails.js
+++ src/pages/user/FacilityDetails.js
@@ -6,120 +6,7 @@ import TimePicker from "rc-time-picker";
 import { useEffect } from "react";
 import { API } from "@aws-amplify/api";
 import * as queries from "../../graphql/queries";
-
-let slots = [
-  {
-    id:"1",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-3"),
-    startTime:"08:00",
-    endTime:"09:00",
-    selected: true,
-    available: true
-  },
-  {
-    id:"2",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-3"),
-    startTime:"12:00",
-    endTime:"13:00",
-    selected: false,
-    available: false
-  },
-  {
-    id:"3",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-3"),
-    startTime:"14:00",
-    endTime:"15:00",
-    selected: false,
-    available: true
-  },
-  {
-    id:"4",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-3"),
-    startTime:"16:00",
-    endTime:"17:00",
-    selected: false,
-    available: true
-  },
-  {
-    id:"5",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-3"),
-    startTime:"18:00",
-    endTime:"19:00",
-    selected: false,
-    available: true
-  },
-  {
-    id:"6",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-3"),
-    startTime:"20:00",
-    endTime:"21:00",
-    selected: false,
-    available: true
-  },
-  {
-    id:"6",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-4"),
-    startTime:"08:00",
-    endTime:"09:00",
-    selected: true,
-    available: true
-  },
-  {
-    id:"7",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-4"),
-    startTime:"10:00",
-    endTime:"11:00",
-    selected: false,
-    available: true
-  },
-  {
-    id:"8",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-4"),
-    startTime:"12:00",
-    endTime:"13:00",
-    selected: false,
-    available: true
-  },
-  {
-    id:"9",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-4"),
-    startTime:"14:00",
-    endTime:"15:00",
-    selected: false,
-    available: true
-  },
-  {
-    id:"10",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-4"),
-    startTime:"16:00",
-    endTime:"17:00",
-    selected: false,
-    available: false
-  },
-  {
-    id:"11",
-    facilityId:"e4ef9825-2b1a-435b-88c3-65d0e767b394",
-    slotDate: new Date("2021-10-4"),
-    startTime:"18:00",
-    endTime:"19:00",
-    selected: false,
-    available: true
-  }
-];
-
-
-
+import moment from "moment";
 
 export default function FacilityDetails(props) {
 
@@ -130,11 +17,11 @@ export default function FacilityDetails(props) {
   const [filteredSlots, setFilteredSlots] = useState([]);
   const [buttons,setButtons] = useState("Please select a date.");
   const [existingBookings, setExistingBookings] = useState([]);
-  //renderSlotOptions();
+
   useEffect(() => {
     async function getBookingList() {
-      
-      console.log("Get booking list :" + facility.id + " " + slotDate.toLocaleDateString());
+      let slotDateString = slotDate.getFullYear() + "-" + ("0"+(slotDate.getMonth()+1)).slice(-2) + "-" + ("0" + slotDate.getDate()).slice(-2);
+      console.log("Get booking list :" + facility.id + " " + slotDateString);
       const getBooking = await API.graphql({
         query: queries.listBookings,
         variables: {
@@ -143,38 +30,94 @@ export default function FacilityDetails(props) {
               eq: facility.id
             },
             booking_date:{
-              eq: slotDate
+              eq: slotDateString,
             }
           },
         },
       });
 
-      //setExistingBookings(getBooking.data.listBookings.items);
-
+      setExistingBookings(getBooking.data.listBookings.items)
       console.log("List of bookings done on this date: ", getBooking);
-      //setExistingBookings([{"fake":123}]);
     };
     getBookingList();
   },[slotDate]);
   useEffect(()=> {
     function fetchSlots(){
-      console.log(existingBookings);
       console.log("fetch slots List of bookings done on this date: ", existingBookings);
       console.log("Date entered: " + slotDate);
       let results = [];
-      for (const x of slots) {
-        if (x.slotDate.toDateString() == slotDate.toDateString() && x.facilityId == facility.id) {
-          results.push(x);
+      let weekday = slotDate.toLocaleString('en-us', {  weekday: 'long' }).substring(0,3);
+      console.log(weekday);
+      console.log("Facility operatin days.", facility.operating_days);
+      for(const operatingDay of facility.operating_days){
+        if (weekday == operatingDay){
+          results = generateSlots(facility.opening_hrs,facility.closing_hrs);
+          break;
         }
+        
       }
+      
       console.log("Results:");
       console.log(results);
       setFilteredSlots(results);
     };
+    function generateSlots(){
+      let startTime = new Date(slotDate);
+      setTime(startTime, facility.opening_hrs);
+      console.log(startTime.toLocaleString());
+      let endTime = new Date(slotDate);
+      setTime(endTime, facility.closing_hrs);
+      console.log(endTime.toLocaleString());
+      
+      if( endTime.getHours() < startTime.getHours() ){
+        return [];
+      }
+      console.log(startTime);
+      let timeStops = [];
+      let count = 0;
+      let tempStartTime = startTime;
+      let tempEndTime = new Date(startTime.toLocaleString());
+      tempEndTime.setHours(tempEndTime.getHours() + 1);
+      while(tempStartTime.getHours() < endTime.getHours()){
+        console.log(tempStartTime.toLocaleString(),tempEndTime.toLocaleString());
+        let isAvailable = true;
+        
+        for(const existingBooking of existingBookings){
+          let existingBookingStart = new Date(slotDate);
+          setTime(existingBookingStart, existingBooking.slot.start_time);
+          let existingBookinEnd = new Date(slotDate);
+          setTime(existingBookinEnd, existingBooking.slot.end_time);
+          console.log(existingBookinEnd,existingBookinEnd);
+          if(tempStartTime >= existingBookingStart && tempStartTime < existingBookinEnd){
+            console.log("Is not available");
+            isAvailable=false;
+          }else if(tempEndTime > existingBookingStart && tempEndTime <= existingBookinEnd){
+            console.log("Is not available");
+            isAvailable=false;
+          }
+        }
+
+        let tempSlot = {
+          id:count,
+          startTime:tempStartTime.toLocaleDateString("en-US",{hour:"2-digit",minute:"2-digit"}).split(',')[1].trim(),
+          enTime:tempEndTime.toLocaleDateString("en-US",{hour:"2-digit",minute:"2-digit"}).split(',')[1].trim(),
+          duration:1,
+          selected:false,
+          available:isAvailable
+        };
+        timeStops.push(tempSlot);
+        count++;
+        tempStartTime.setHours(tempStartTime.getHours() + 1);
+        tempEndTime.setHours(tempEndTime.getHours() + 1);
+      }
+      return timeStops;
+    }
+    
     fetchSlots();
   },[existingBookings]);
 
   useEffect(() => {
+    console.log("filtered slots changed");
     function getSlotButtonClass(slotSelected, slotAvailable){
 
       if(!slotAvailable){
@@ -188,79 +131,46 @@ export default function FacilityDetails(props) {
   
     }
  
-    function toggleSlotSelection(slot){
-      console.log("Toggle Slot Selection")
-      console.log("slot selected: ");
-  
-      let results = [];
-      for (const x of slots) {
-        if (x.slotDate.toDateString() == slot.slotDate.toDateString() && x.facilityId == slot.facilityId && slot.startTime == x.startTime) {
-          console.log(x)
-        }
-      }
-  
-    }
+    
     let buttonsTemp = 'Please Select a date.';
     if (filteredSlots== null || filteredSlots.length == 0) {
       buttonsTemp = <p>Sorry. There are no slots available.</p>;
     } else {
-      buttonsTemp = <ul>{filteredSlots.map((slot,i) => <button disabled={!slot.available} onClick={function(e) {toggleSlotSelection(slot);}} className={getSlotButtonClass(slot.selected, slot.available)}>
+      buttonsTemp = <ul>{filteredSlots.map((slot,i) => <button disabled={!slot.available} onClick={function(e) {toggleSlotSelection(slot.id);}} className={getSlotButtonClass(slot.selected, slot.available)}>
         {slot.startTime}
       </button>)}</ul>;
     }
     setButtons(buttonsTemp)
   },[filteredSlots])
-  // function renderSlotOptions () {
-  //   let results = fetchSlots("e4ef9825-2b1a-435b-88c3-65d0e767b394",slotDate);
-  //   setFilteredSlots(results);
-  //   console.log("Filtered:");
-  //   console.log(filteredSlots);
-  //   let buttonsTemp = 'Please Select a date.';
-  //   if (filteredSlots== null || filteredSlots.length == 0) {
-  //     buttonsTemp = <p>Sorry. There are no slots available.</p>;
-  //   } else {
-  //     buttonsTemp = <ul>{filteredSlots.map((slot,i) => <button onClick={function(e) {toggleSlotSelection(slot);}} className={getSlotButtonClass(slot.selected, slot.available)}>
-  //       {slot.startTime}
-  //     </button>)}</ul>;
-  //   }
-  //   setButtons(buttonsTemp)
-  // }
-  // function fetchSlots(facilityId, date){
-  //   let results = [];
-  //   for (const x of slots) {
-  //     if (x.slotDate.toDateString() == date.toDateString() && x.facilityId == facilityId) {
-  //       results.push(x);
-  //     }
-  //   }
-  //   console.log("Results:");
-  //   console.log(results);
-  //   return results;
-  // }
-  // function getSlotButtonClass(slotSelected, slotAvailable){
-  //   console.log("is slot selected?");
-  //   console.log(slotSelected, slotAvailable);
-  //   if(!slotAvailable){
-  //     return "bg-gray-500 text-gray-700 font-semibold py-2 px-4 border border-blue-500  rounded margin-top-100px"
-  //   }
-  //   else if(slotSelected){
-  //     return "bg-blue-200 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded margin-top-100px"
-  //   }else{
-  //     return "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded margin-top-100px"
-  //   }
 
-  // }
-  // function toggleSlotSelection(slot){
-  //   console.log("Toggle Slot Selection")
-  //   console.log("slot selected: ");
+  function setTime(date,timeString) {
+    date.setHours(timeString.substring(0, 2));
+    date.setMinutes(timeString.substring(3, 5));
+  }
+  function toggleSlotSelection(slotId){
+    console.log("Toggle Slot Selection")
+    console.log("slot selected: ");
+    let tempFilteredSlots = [];
+    for(const slot of filteredSlots){
+      if(slot.id == slotId){
+        if(slot.selected == true){
+          slot.selected = false;
+        }else{
+          slot.selected = true;
+        }
+      }else{
+        slot.selected = false;
+      }
+      tempFilteredSlots.push(slot);
+    }
+    
+    
+    setFilteredSlots(tempFilteredSlots);
+    console.log(filteredSlots);
+    
 
-  //   let results = [];
-  //   for (const x of slots) {
-  //     if (x.slotDate.toDateString() == slot.slotDate.toDateString() && x.facilityId == slot.facilityId && slot.startTime == x.startTime) {
-  //       console.log(x)
-  //     }
-  //   }
+  }
 
-  // }
   
   
 
@@ -326,6 +236,8 @@ export default function FacilityDetails(props) {
       <Footer />
     </div>
   );
+
+
 }
 
 
