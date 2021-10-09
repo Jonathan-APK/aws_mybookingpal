2f60b1630dfaf1da7d8fd91706a7907b934eb4ec
Poh Keong <angpkeong@gmail.com>
2021-10-09 22:42:24 +0800

    Test devops

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -615,7 +615,7 @@ function Landing() {
                 Facility Booking / Renting
               </h2>
               <p className="mt-2 pb-5 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                Feature-packed, simple to use.
+                Feature-packed, Simple to use. Facility at your fingertips.
               </p>
             </div>
 
