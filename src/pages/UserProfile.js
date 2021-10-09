d9d202e947c1c14b966e67712ea8001ad7c27485
UzKhanBAK <muzairk.11@gmail.com>
2021-10-09 22:15:12 +0800

    Edit profile function

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { connect } from "react-redux";
 import Footer from "../components/layout/Footer";
 import UserNavbar from "../components/layout/navbar/UserNavbar";
 import DefaultUserPic from "../uploads/team-male.jpg";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Form, Container, Row, Col } from "react-bootstrap";
 import { API } from "@aws-amplify/api";
 import * as queries from "../graphql/queries";
-import UploadPicModal from "../components/user/UploadPicModal";
-import EditProfile from "../components/user/EditProfile";
+import EditProfileModal from "../components/user/EditProfileModal";
 import { useState, useEffect } from "react";
-import axios from 'axios';
 
 const profiles = [
   {
@@ -20,19 +17,16 @@ const profiles = [
     email: "maria_ozawa@gmail.com",
     contact: "96712341",
     dob: "09-08-1965",
-    address: "NCS Building"
+    address: "NCS Building",
   },
   // More booking...
 ];
 
+export default function UserProfile() {
+  const profilePic = DefaultUserPic;
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+  const [userData, setUserData] = useState([]);
 
-
-  export default function UserProfile() {
-
-    const profilePic = DefaultUserPic;
-
-    const [userData, setUserData] = useState([]);
-  
   useEffect(() => {
     async function getUser() {
       const getUser = await API.graphql({
@@ -45,149 +39,174 @@ const profiles = [
           },
         },
       });
-      // setUserData(getUserData.data.listUsers);
+      setUserData(getUser.data.listUsers.items);
     }
     getUser();
   }, []);
 
-
-    return (
+  return (
+    <div>
+      <UserNavbar />
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">Manage Profile</h1>
+        </div>
+      </header>
       <div>
-        <UserNavbar />
-        <header className="bg-white shadow">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-gray-900">Manage Profile</h1>
-          </div>
-        </header>
-        <div>
-          <section className="text-blueGray-700 ">
-            <div className="container flex flex-col items-center px-5 py-16 mx-auto md:flex-row lg:px-28">
-              <div className="flex flex-col items-start mb-16 text-left lg:flex-grow md:w-1/4 lg:pr-24 md:pr-16 md:mb-0">
+        <EditProfileModal
+          isOpen={isEditModalOpen}
+          setModalOpen={setEditModalOpen}
+          user={userData}
+        />
+        <section className="text-blueGray-700 ">
+          <div className="container flex flex-col items-center px-5 py-16 mx-auto md:flex-row lg:px-28">
+            <div className="flex flex-col items-start mb-16 text-left lg:flex-grow md:w-1/4 lg:pr-24 md:pr-16 md:mb-0">
+              <div>
                 <div>
-                  <div>
-                    <a href="#">
-                      <img
-                        className="h-30 w-30 rounded-full "
-                        alt="profile pic"
-                        src={profilePic}
-                      />
-                    </a>
-                  </div>
-                  <div className="py-5 margin: text-center">
-                    <input type="file" onChange="" />
-                    <button className="ui button teal" onClick="">Save</button>
-                  </div>
+                  <a href="#">
+                    <img
+                      className="h-30 w-30 rounded-full "
+                      alt="profile pic"
+                      src={profilePic}
+                    />
+                  </a>
+                </div>
+                <div className="py-5 margin: text-center">
+                  <input type="file" onChange="" />
+                  <button className="ui button teal" onClick="">
+                    Save
+                  </button>
                 </div>
               </div>
-              <div className="flex flex-col items-start mb-16 text-left lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:mb-0">
-                {userData.map((user) => (
-                  <Container>
-                    <Row>
-                      <Col>
-                        <Form>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="formPlaintextFirstName"
-                          >
-                            <Form.Label column sm="2">
-                              First Name
-                            </Form.Label>
-                            <Col sm="10">
-                              <Form.Control
-                                plaintext
-                                readOnly
-                                defaultValue={user.firstname}
-                              />
-                            </Col>
-                          </Form.Group>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="formPlaintextLastName"
-                          >
-                            <Form.Label column sm="2">
-                              Last Name
-                            </Form.Label>
-                            <Col sm="10">
-                              <Form.Control
-                                plaintext
-                                readOnly
-                                defaultValue={user.lastName}
-                              />
-                            </Col>
-                          </Form.Group>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="formPlaintextEmail"
-                          >
-                            <Form.Label column sm="2">
-                              Email
-                            </Form.Label>
-                            <Col sm="10">
-                              <Form.Control
-                                plaintext
-                                readOnly
-                                defaultValue={user.email}
-                              />
-                            </Col>
-                          </Form.Group>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="formPlaintextEmail"
-                          >
-                            <Form.Label column sm="2">
-                              Contact Number
-                            </Form.Label>
-                            <Col sm="10">
-                              <Form.Control
-                                plaintext
-                                readOnly
-                                defaultValue={user.contact}
-                              />
-                            </Col>
-                          </Form.Group>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="formPlaintextEmail"
-                          >
-                            <Form.Label column sm="2">
-                              Date Of Birth
-                            </Form.Label>
-                            <Col sm="10">
-                              <Form.Control
-                                plaintext
-                                readOnly
-                                defaultValue={user.dob}
-                              />
-                            </Col>
-                          </Form.Group>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="formPlaintextEmail"
-                          >
-                            <Form.Label column sm="2">
-                              Address
-                            </Form.Label>
-                            <Col sm="10">
-                              <Form.Control
-                                plaintext
-                                readOnly
-                                defaultValue={user.address}
-                              />
-                            </Col>
-                          </Form.Group>
-                        </Form>
-                      </Col>
-                    </Row>
-                  </Container>
-                ))}
-              </div>
             </div>
-          </section>
-        </div>
-        <Footer />
+            <div className="flex flex-col items-start mb-16 text-left lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:mb-0">
+              {userData.map((user) => (
+                <Container>
+                  <Row>
+                    <Col>
+                      <Form>
+                        <Form.Group
+                          className="mb-3"
+                          controlId="formPlaintextFirstName"
+                        >
+                          <Form.Label column sm="2">
+                            First Name
+                          </Form.Label>
+                          <Col sm="10">
+                            <Form.Control
+                              plaintext
+                              readOnly
+                              defaultValue={user.firstname}
+                            />
+                          </Col>
+                        </Form.Group>
+                        <Form.Group
+                          className="mb-3"
+                          controlId="formPlaintextLastName"
+                        >
+                          <Form.Label column sm="2">
+                            Last Name
+                          </Form.Label>
+                          <Col sm="10">
+                            <Form.Control
+                              plaintext
+                              readOnly
+                              defaultValue={user.lastName}
+                            />
+                          </Col>
+                        </Form.Group>
+                        <Form.Group
+                          className="mb-3"
+                          controlId="formPlaintextEmail"
+                        >
+                          <Form.Label column sm="2">
+                            Contact Number
+                          </Form.Label>
+                          <Col sm="10">
+                            <Form.Control
+                              plaintext
+                              readOnly
+                              defaultValue={user.contact}
+                            />
+                          </Col>
+                        </Form.Group>
+                        <Form.Group
+                          className="mb-3"
+                          controlId="formPlaintextEmail"
+                        >
+                          <Form.Label column sm="2">
+                            Email
+                          </Form.Label>
+                          <Col sm="10">
+                            <Form.Control
+                              plaintext
+                              readOnly
+                              defaultValue={user.email}
+                            />
+                          </Col>
+                        </Form.Group>
+                        <Form.Group
+                          className="mb-3"
+                          controlId="formPlaintextEmail"
+                        >
+                          <Form.Label column sm="2">
+                            Address
+                          </Form.Label>
+                          <Col sm="10">
+                            <Form.Control
+                              plaintext
+                              readOnly
+                              defaultValue={user.address}
+                            />
+                          </Col>
+                        </Form.Group>
+                        <Form.Group
+                          className="mb-3"
+                          controlId="formPlaintextEmail"
+                        >
+                          <Form.Label column sm="2">
+                            Date Of Birth
+                          </Form.Label>
+                          <Col sm="10">
+                            <Form.Control
+                              plaintext
+                              readOnly
+                              defaultValue={user.dob}
+                            />
+                          </Col>
+                        </Form.Group>
+                      </Form>
+                    </Col>
+                    <div>
+                      <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex"
+                        onClick={() => setEditModalOpen(true)}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                          />
+                        </svg>
+                        <span className="ml-1">Edit Profile</span>
+                      </button>
+                    </div>
+                  </Row>
+                </Container>
+              ))}
+            </div>
+          </div>
+        </section>
       </div>
-    );
-  }
-
-
-
+      <Footer />
+    </div>
+  );
+}
