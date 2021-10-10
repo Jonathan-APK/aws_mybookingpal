ef1c353e72d453ae7ac8987f03e82d5fb8072124
Vincent Lim <helloimvincent@gmail.com>
2021-10-10 18:22:20 +0800

    - Update user facility list (show 20 records and additional operating days) - Add pagination for facility owner manage page

--- src/components/user/FacilityListItem.js
+++ src/components/user/FacilityListItem.js
@@ -2,12 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function FacilityListItem(props) {
   return (
-    <div className="container flex border bg-white h-48 p-5 rounded-lg shadow-lg relative hover:shadow-2xl hover:bg-blue-200 transition duration-500">
+    <div className="flex border bg-white h-48 p-5 rounded-lg shadow-lg relative hover:shadow-2xl hover:bg-blue-200 transition duration-500">
       <img src={props.imageSrc} className="rounded w-40 h-auto" />
       <div className="max-w-xl w-full px-5">
         <h1 className="text-2xl text-gray-800 font-semibold">{props.facility.name}</h1>
         <h2 className="text-gray-700 font-medium">{props.facility.address}</h2>
-        <h3 className="text-gray-700 font-medium -mt-1.5 mb-2">
+        <h3 className="text-gray-700 -mt-1.5">
+          Operating Days: {props.facility.operating_days.join(", ")}
+        </h3>
+        <h3 className="text-gray-700 -mt-1.5 mb-2">
           Operating Hours: {props.facility.opening_hrs} - {props.facility.closing_hrs}
         </h3>
         <p className="truncate text-gray-600 text-sm leading-tight tracking-normal">
